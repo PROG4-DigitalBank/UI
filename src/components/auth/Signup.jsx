e077699dbd20c805ebb70b8232a1250ca8dce0ae
Karenathi <hei.karenandriantasy@gmail.com>
2024-03-18 19:54:26 +0300

    feat: navbar animation

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -49,7 +49,7 @@ const Signup = () => {
       <div className="all">
         <nav className="navbar">
           <div className="left-links">
-            <Link to="/business" className="link">
+            <Link to="/business text-pop-up-top" className="link">
               Business
             </Link>
             <Link to="/pricing" className="link">
@@ -64,7 +64,7 @@ const Signup = () => {
           </div>
           <div className="right-links">
             <button className="up-button">Login</button>
-            <button className="up-button">Signup</button>
+            <button className="up-button" id='signup-button'>Signup</button>
           </div>
         </nav>
 
