eb2c99ffe528ae27ca5fcb4eeea6f830642a1547
Karenathi <hei.karenandriantasy@gmail.com>
2024-03-20 14:39:22 +0300

    feat: add route for balance replenishment

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Signup from './components/auth/Signup'
 import Login from './components/auth/Login'
 import BalanceInquiry from './components/features/BalanceInquiry'
 import WithdrawMoney from './components/features/WithdrawMoney'
+import BalanceReplenishment from './components/features/BalanceReplenishment'
 function App() {
   return (
     <Router>
@@ -15,6 +16,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/wmoney" element={<WithdrawMoney />} />
           <Route path="/balance" element={<BalanceInquiry />} />
+          <Route path="/balancerep" element={<BalanceReplenishment />} />
+        
         </Routes>
       </div>
     </Router>
