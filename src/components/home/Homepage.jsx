92a94a046da05202b9faa5d82c2c9cf1f1628323
Karenathi <hei.karenandriantasy@gmail.com>
2024-03-21 18:32:22 +0300

    feat: homepage global design

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './styles/home.css'
 import { Link } from 'react-router-dom'
+import side from './images/side-image.jpg'
 
 function Homepage() {
   return (
@@ -10,18 +11,48 @@ function Homepage() {
           <Link to="/">
             <h1 className="logo">Digital Bank</h1>
           </Link>
-          <Link to="/" className='link-'>Account Info</Link>
-          <Link to="/"className='link-'>Withdraw</Link>
-          <Link to="/"className='link-'>Balance</Link>
-          <Link to="/"className='link-'>Deposit</Link>
-          <Link to="/"className='link-'>Transfer</Link>
+          <Link to="/" className="link-">
+            Account Info
+          </Link>
+          <Link to="/" className="link-">
+            Withdraw
+          </Link>
+          <Link to="/" className="link-">
+            Balance
+          </Link>
+          <Link to="/" className="link-">
+            Deposit
+          </Link>
+          <Link to="/" className="link-">
+            Transfer
+          </Link>
         </div>
 
         <div className="buttons-container">
-          <button>Login</button>
-          <button>Sign Up</button>
+          <button className='try' >Login</button>
+          <button className='learn' >Sign Up</button>
         </div>
       </div>
+
+      <main>
+        <div>
+          <div className='side-text'>
+            <h1>One app, all things money</h1>
+            <p>
+              From easily money management to travel perks and investments. Open
+              your account in a flash
+            </p>
+          </div>
+          <div className='main-buttons'>
+            <button className='try' id='try'>Try Now</button>
+            <button className='learn' id='learn'>Learn More</button>
+          </div>
+        </div>
+
+        <div>
+            <img src={side} alt="" className='side-image' />
+        </div>
+      </main>
     </>
   )
 }
